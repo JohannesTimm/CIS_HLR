6621958d0903865c189d42974dd98a16ee1cc11d
Michael Hufschmidt <michael@hufschmidt-web.de>
2014-10-17 17:32:07 +0200

    Aufgabe 2 fertig

--- HufschmidtTimm/map.c
+++ HufschmidtTimm/map.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 // Definieren Sie ein enum cardd
 typedef enum { X = 0, N = 1, S = 2, E = 4, W = 8} cardd;
 
 // Definieren Sie ein 3x3-Array namens map, das Werte vom Typ cardd enthält
-cardd* map[3][3] = {{X, X, X}, {X, X, X}, {X, X, X}};
+cardd map[3][3] = {{X, X, X}, {X, X, X}, {X, X, X}};
+
+bool valid_dir(cardd dir) {
+  bool ok;
+  ok = (dir == N) || (dir == S) || (dir == E) || (dir == W) ||
+       (dir == (N | W)) || (dir == (S | W)) ||
+       (dir == (N | E)) || (dir == (S | E));
+  return ok;
+}
 
 // Die Funktion set_dir soll an Position x, y den Wert dir in das Array map eintragen
 // Überprüfen Sie x und y um mögliche Arrayüberläufe zu verhindern
@@ -16,51 +25,95 @@ void set_dir (int x, int y, cardd dir)
   int d = (int) dir;
   if ((x < 0) | (x > 2)) {
     printf ("Ungültiger Wert %d für x\n", x);
-    // exit(EXIT_FAILURE);
     return;
   }
   if ((y < 0) | (y > 2)) {
     printf ("Ungültiger Wert %d für y\n", y);
-    // exit(EXIT_FAILURE);
     return;
   }
-  if ((d < 0) | (d > 15) | (d == 3) | (d == 12)) {
+  if (!valid_dir(dir)) {
     printf ("Ungültiger Wert %d für d\n", d);
-    // exit(EXIT_FAILURE);
     return;
   }
-  map[x][y] = dir; // Kompilierungs-Fehler
+  // printf("x = %d, y = %d, (int)dir = %d\n", x, y, d); // only for test
+  map[x][y] = dir;
 }
 
 // Die Funktion show_map soll das Array in Form einer 3x3-Matrix ausgeben
-void show_map (void)
-{
-  int row, col;
-  cardd dir;
-  char* singleLetters = "NSEW";
-  for (row = 0; row < 2; row++) {
-    for (col = 0; col < 2; col++) {
-      dir = map[row][col]; // Kompilierungs-Fehler
-      switch (dir) {
-        case N:
-          //
-        break;
-        case S:
-        //
-        break;
-        case E:
-        //
-        break;
-        case W:
-        //
-        break;
-        default:
-        //
-        break;
+void show_map (void) {
+  int row, col, pos, num;
+  char const_zeile[] = "         ", zeile[10];
+  for (row = 0; row < 3; row++) {
+    strcpy(zeile, const_zeile);  // Leere Zeile
+    pos = 0;
+    for (col = 0; col < 3; col++) {
+      num = (int) map[row][col];
+      switch (num) {
+        case 0:
+          zeile[pos] = 'O';
+          break;
+        case 1:
+          zeile[pos] = 'N';
+          break;
+        case 2:
+          zeile[pos] = 'S';
+          break;
+        case 4:
+          zeile[pos] = 'E';
+          break;
+        case 8:
+          zeile[pos] = 'W';
+          break;
+        case 5:
+          if (pos == 0) {
+            zeile[pos] = 'N';
+            zeile[pos + 1] = 'E';
+          } else {
+            zeile[pos-1] = 'N';
+            zeile[pos] = 'E';
+          }
+          break;
+        case 9:
+          if (pos == 0) {
+            zeile[pos] = 'N';
+            zeile[pos + 1] = 'W';
+          } else {
+            zeile[pos-1] = 'N';
+            zeile[pos] = 'W';
+          }
+          break;
+        case 6:
+          if (pos == 0) {
+            zeile[pos] = 'S';
+            zeile[pos + 1] = 'E';
+          } else {
+            zeile[pos-1] = 'S';
+            zeile[pos] = 'E';
+          }
+          break;
+        case 10:
+          if (pos == 0) {
+            zeile[pos] = 'S';
+            zeile[pos + 1] = 'W';
+          } else {
+            zeile[pos-1] = 'S';
+            zeile[pos] = 'W';
+          }
+          break;
+      } //  end switch
+      /* Just for testing
+      printf ("row = %d, col = %d, d = %d, dir = ", row, col, num);
+      if (pos == 0) {
+        printf("%c%c\n", zeile[pos], zeile[pos+1]);
+      } else {
+        printf("%c%c\n", zeile[pos-1], zeile[pos]);
       }
-    }
-  }
-}
+      */
+      pos = pos + 4;
+    }  // end for col
+    printf("%s\n", zeile);
+  }  // end for row
+}  // end show_map
 
 int main (void)
 {
